fc0222a1bc5679c77f2bbe7e1483e742fa4ca649
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-05-07 18:16:43 +0200

    input controls

--- core/src/main/java/com/rubenbellido/drop1/Main.java
+++ core/src/main/java/com/rubenbellido/drop1/Main.java
@@ -1,9 +1,12 @@
 package com.rubenbellido.drop1;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
@@ -26,6 +29,10 @@ public class Main extends ApplicationAdapter {
     Sound dropSound;
     Music music;
 
+    // input controls
+    Sprite bucketSprite; // Declare a new Sprite variable
+    Vector2 touchPos;
+
     @Override
     public void create() {
         // rendering
@@ -38,6 +45,11 @@ public class Main extends ApplicationAdapter {
         dropTexture = new Texture("drop.png");
         dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
         music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+
+        // input controls
+        bucketSprite = new Sprite(bucketTexture); // Initialize the sprite based on the texture
+        bucketSprite.setSize(1, 1); // Define the size of the sprite
+        touchPos = new Vector2();
     }
 
     @Override
@@ -49,7 +61,20 @@ public class Main extends ApplicationAdapter {
     }
 
     private void input() {
+        float speed = 4f;
+        float delta = Gdx.graphics.getDeltaTime();
 
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            bucketSprite.translateX(speed * delta);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            bucketSprite.translateX(-speed * delta);
+        }
+
+        if (Gdx.input.isTouched()) {
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY()); // Get where the touch happened on screen
+            viewport.unproject(touchPos); // Convert the units to the world units of the viewport
+            bucketSprite.setCenterX(touchPos.x); // Change the horizontally centered position of the bucket
+        }
     }
 
     private void logic() {
@@ -65,9 +90,8 @@ public class Main extends ApplicationAdapter {
         float worldWidth = viewport.getWorldWidth();
         float worldHeight = viewport.getWorldHeight();
 
-        // rearrange these two lines
-        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight); // draw the background
-        spriteBatch.draw(bucketTexture, 0, 0, 1, 1); // draw the bucket
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+        bucketSprite.draw(spriteBatch); // Sprites have their own draw method
 
         spriteBatch.end();
     }
