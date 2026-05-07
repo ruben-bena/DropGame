38b1911f467c31abfff03b8f1cc847f76a1eb05f
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-05-07 18:13:25 +0200

    render

--- core/src/main/java/com/rubenbellido/drop1/Main.java
+++ core/src/main/java/com/rubenbellido/drop1/Main.java
@@ -1,6 +1,7 @@
 package com.rubenbellido.drop1;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -10,11 +11,13 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    // rendering
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
 
     // assets
     Texture backgroundTexture;
@@ -25,8 +28,9 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        // rendering
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(8, 5);
 
         // assets
         backgroundTexture = new Texture("background.png");
@@ -38,15 +42,38 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        // organize code into three methods
+        input();
+        logic();
+        draw();
+    }
+
+    private void input() {
+
+    }
+
+    private void logic() {
+
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        // rearrange these two lines
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight); // draw the background
+        spriteBatch.draw(bucketTexture, 0, 0, 1, 1); // draw the bucket
+
+        spriteBatch.end();
     }
 
     @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
+    public void resize(int width, int height) {
+        viewport.update(width, height, true); // true centers the camera
     }
 }
